abcbcdecfb0e192395973c378e49a51d1badff4e
rafaelcetina <rafael.cetina@hotmail.com>
2016-11-18 09:07:44 -0500

    18/11/2016

--- app/Sop_cargas_arribo.php
+++ app/Sop_cargas_arribo.php
@@ -9,23 +9,12 @@ class Sop_cargas_arribo extends Model
    protected $primaryKey = 'CARR_ID'; // or nul
    
    //use SoftDeletes;
-
-   //protected $dateFormat = 'Y-d-m H:i:s';
-
    
    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
-   /*
-   Schema::table('SOP_MUELES', function ($table) {
-    $table->softDeletes();
-	});
-	*/
+   
 
 	protected $casts = [
         'SARR_PELIGRO' => 'bit',
-        // 'SARR_ETA' => 'array',
-        // 'SARR_ETB' => 'array',
-        // 'SARR_ETD' => 'array',
-        // 'SARR_HISTORIAL_PUERTOS' => 'array',
     ];
 
 }
